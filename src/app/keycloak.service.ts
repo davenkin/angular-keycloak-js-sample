1e227a6ad587c4cc63a14da1382fecfc649ef05d
Teng Yun <davenkin@126.com>
2025-10-08 10:43:40 +0800

    added doc

--- src/app/keycloak.service.ts
+++ src/app/keycloak.service.ts
@@ -16,7 +16,8 @@ export class KeycloakService {
   init() {
     // refresh the access token upon previous token expired
     // if the refresh operation response error(such as refresh token expired which results in 400 error), it calls Keycloak.clearToken() internally
-    // Keycloak.clearToken() will call onAuthLogout, and if configured as `login-required`, it will do login() which shows the login page
+    // Keycloak.clearToken() will call onAuthLogout, and if configured as `login-required`, it will automatically call login() which shows the login page
+    // so there is no need to explicitly call Keycloak.login() by yourself
     this.keycloak.onTokenExpired = () => {
       this.keycloak.updateToken();
     };
